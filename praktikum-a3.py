554186df5263079ef055ed69d8e835e48819a85f
alfrzkyysf <153788053+alfrzkyysf@users.noreply.github.com>
2023-12-14 09:59:25 +0700

    upload praktikum a3

---
+++ praktikum-a3.py
@@ -0,0 +1,371 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON
+N SIR ISSAC
+NEWTON S I
+
+&SIR @ISSAC# *NEWTON.
+SIR ISSAC NEWTON
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON
+['SIR', 'ISSAC', 'NEWTON']
+N SIR ISSAC
+N SIR ISSAC
+NEWTON S I
+
+&SIR @ISSAC# *NEWTON.
+SIR ISSAC NEWTON
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON
+['SIR', 'ISSAC', 'NEWTON']
+N SIR ISSAC
+
+&SIR @ISSAC# *NEWTON.
+SIR ISSAC NEWTON
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+Traceback (most recent call last):
+  File "C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py", line 42, in <module>
+    print(up[-1][0],''.up[0:-1])
+AttributeError: 'str' object has no attribute 'up'
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIRISSACNEWTON
+SIR ISSAC NEWTON
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+Traceback (most recent call last):
+  File "C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py", line 47, in <module>
+    up = up.split()
+AttributeError: 'list' object has no attribute 'split'
+clear
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIRISSACNEWTON
+F SIR ISSAC NEWTON
+FRANKEL S I
+NEWTON S I
+
+&SIR @ISSAC# *NEWTON.
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIR ISSAC NEWTON
+F SIR ISSAC NEWTON
+FRANKEL S I
+NEWTON S I
+
+&SIR @ISSAC# *NEWTON.
+
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIR ISSAC NEWTON
+F SIR ISSAC NEWTON
+FRANKEL S I
+NEWTON S I
+
+&SIR$ @ISSAC# *NEWTON.
+&  
+ S I
+SIR ISSAC NEWTON
+
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIR ISSAC NEWTON
+F SIR ISSAC NEWTON
+FRANKEL S I
+NEWTON S I
+
+&SIR$ @ISSAC# *NEWTON.
+& ISSAC NEWTON
+SIR ISSAC NEWTON
+SIR ISSAC NEWTON
+>>> 
+= RESTART: C:\Users\user\Documents\pak-zaitun\program-a-231031030-alfarizky\praktikum-a3.py
+----------------------------------------
+           MUHAMMAD ALFARIZKY           
+               231031030                
+
+
+
+                             Praktikum 3
+                        Sistem Informasi
+              banditberpeci011@gmail.com
+----------------------------------------
+ Halo,nama saya Muhammad Alfarizky dengan NIM 231031030  dari prodi Sistem Informasi,ini adalah  Praktikum 3.Terima kasih.
+Biodata saya:
+Nama   	: Muhammad Alfarizky
+NIM    	: 231031030
+meet   	: praktikum 3
+Prodi  	: Sistem Informasi
+email  	: Banditberpeci011@Gmail.Com
+TTL    	: parepare,11-maret-2005
+Alamat 	: Perumahan Grand Sulawesi Blok J2
+Asal   	: Parepare
+Hobi   	: Bermain Futsal
+Tinngi 	: 165 cm
+
+SIR ISSAC NEWTON FRANKEL
+['SIR', 'ISSAC', 'NEWTON', 'FRANKEL']
+F SIR ISSAC NEWTON
+F SIR ISSAC NEWTON
+FRANKEL S I
+NEWTON S I
+
+&SIR$ @ISSAC# *NEWTON.
+SIR ISSAC NEWTON
+SIR ISSAC NEWTON
+SIR ISSAC NEWTON
