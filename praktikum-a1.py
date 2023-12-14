99578b1263549473c6349d186eed6b75d41adf8a
alfrzkyysf <153788053+alfrzkyysf@users.noreply.github.com>
2023-12-14 09:48:17 +0700

    upload praktikum a1

---
+++ praktikum-a1.py
@@ -0,0 +1,95 @@
+print('praktikum-1')
+print('Nam a Lengkap : Muhammad Alfarizky Yusuf')
+print('Nim           :231031030')
+print('Prodi         :Sistem Informasi')
+print()
+x=2023
+
+print('Angkatan',x)
+print()
+a=10
+print('data adalah: ',a)
+print('tipe data adalah',type(a))
+print()
+
+b=10.0
+print()
+print('data adalah: ',b)
+print('tipe data adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('data adalah: ',kampus)
+print('tipe data adalah',type(kampus))
+print()
+
+c=complex(2,3)
+print('data adalah: ',c)
+print('tipe data adalah',type(c))
+print()
+
+a=10
+print('data adalah: ',a)
+print('tipe data adalah',type(a))
+print()
+
+log = True
+print('data adalah: ',log)
+print('tipe data adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p + q
+print('hasil',p,'+',q,'=',hasil)
+print()
+
+#tugas pratikum
+print()
+#pertambahan 
+s=10
+d=5
+hasil = s + d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s + d adalah {hasil}')
+print()
+
+#pengurangan 
+s=10
+d=5
+hasil = s - d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s - d adalah {hasil}')
+print()
+
+#perkalian
+s=10
+d=5
+hasil = s * d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s * d adalah {hasil}')
+print()
+
+#pembagian
+s=10
+d=5
+hasil = s / d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s / d adalah {hasil}')
+print()
+
+#modulus
+s=10
+d=5
+hasil = s % d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s % d adalah {hasil}')
+print()
+
+#eksponensial
+s=10
+d=5
+hasil = s ** d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s ** d adalah {hasil}')
+print()
+
+#floor division
+s=10
+d=5
+hasil = s // d
+print(f'nilai s adalah {s}, dan d adalah {d}, maka s // d adalah {hasil}')
